7c987fc908da91c070200450711239a342bceb62
Johny A.  Pedraza Romero <johnyantonio892@gmail.com>
2026-01-15 23:31:00 -0500

    feat: add interface api

---
+++ src/api/api.inteface.ts
@@ -0,0 +1,48 @@
+import { ServiceError } from "../exceptions/exceptions";
+
+export interface APIRequest<Query = any, Body = any, Params = any> {
+  query: Query;
+  body: Body;
+  params: Params;
+}
+
+export interface APIResponse<Data = any> {
+  status(code: number): this;
+  json(data: Data): void;
+}
+
+export interface IAPIHandler<
+  T extends { id: number | string },
+  CreateDto = Omit<T, "id">,
+  UpdateDto = Partial<T>,
+  ResponseDto = T,
+  E = ServiceError
+> {
+  findAll(req: APIRequest, res: APIResponse<ResponseDto[]>): Promise<void>;
+  findById(
+    req: APIRequest<{}, {}, { id: string | number }>,
+    res: APIResponse<ResponseDto | null>
+  ): Promise<void>;
+  findOne(req: APIRequest, res: APIResponse<ResponseDto | null>): Promise<void>;
+  create(
+    req: APIRequest<{}, CreateDto>,
+    res: APIResponse<ResponseDto>
+  ): Promise<void>;
+  update(
+    req: APIRequest<{}, UpdateDto, { id: string | number }>,
+    res: APIResponse<ResponseDto | undefined>
+  ): Promise<void>;
+  hardDelete(
+    req: APIRequest<{}, {}, { id: string | number }>,
+    res: APIResponse<boolean>
+  ): Promise<void>;
+  softDelete(
+    req: APIRequest<{}, {}, { id: string | number }>,
+    res: APIResponse<boolean>
+  ): Promise<void>;
+  restore(
+    req: APIRequest<{}, {}, { id: string | number }>,
+    res: APIResponse<boolean>
+  ): Promise<void>;
+  count(req: APIRequest, res: APIResponse<number>): Promise<void>;
+}
